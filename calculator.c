542c47527716e6ea4f5fac8bcbdfa73f90450f92
tabbyisntacat <107060859+tabbyisntacat@users.noreply.github.com>
2022-08-31 18:51:33 -0500

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("How many eggs do you have: ");
+    int cans;
+    scanf("%i", &cans);
+    double dozen = cans / 12;
+    printf("you have : %f dozens of eggs\n",dozen);
+  return 0;
+}
