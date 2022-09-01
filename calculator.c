386a697af7e063b37bb49c1c4da9b07d183d6419
tabbyisntacat <107060859+tabbyisntacat@users.noreply.github.com>
2022-08-31 19:04:00 -0500

    Fixxed a issue,

--- calculator.c
+++ calculator.c
@@ -4,7 +4,7 @@ int main(){
     printf("How many eggs do you have: ");
     int cans;
     scanf("%i", &cans);
-    double dozen = cans / 12;
+    double dozen = (double) cans / 12;
     printf("you have : %f dozens of eggs\n",dozen);
   return 0;
 }
